09c6a3b9aa61d63ad2343a3d6b62b32af83b4282
AlexianeBA <barbealexiane07@gmail.com>
2023-04-12 20:09:43 +0200

    finish to create fetch
    create element for search informations from api

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -5,11 +5,30 @@ function afficher_object() {
       // Traiter les données JSON
       //définir les éléments qu'on va utiliser présents dans l'api
       const data_response = data;
-      data_response.forEach((element) => {
-        //faire appel à la div gallery afin d'afficher les informations dans data_response
-        document.getElementById("gallery").innerHTML = element.title;
-      });
-    })
-    .catch((error) => console.error(error));
+      const gallery = document.getElementById("gallery");
+      data_response
+        // pour chaque éléments on donne le chemin vers html
+        .forEach((element) => {
+          //on créé le parent figure
+          var figure = document.createElement("figure");
+          // on créé l'enfant de figure : img
+          var img = document.createElement("img");
+          // on attribut à img sa source
+          img.src = element.imageUrl;
+          // on attribut à img son titre
+          img.title = element.title;
+          // on créé l'enfant de figure : figcaption
+          var figcaption = document.createElement("figcaption");
+          figcaption.textContent = element.title;
+          //on attribu à figure son enfant img
+          figure.appendChild(img);
+          // on attribut à figure son enfant figcaption
+          figure.appendChild(figcaption);
+          // on attribut à gallery son enfant figure
+          gallery.appendChild(figure);
+        })
+        //afficher error si la recherche dans l'api n'aboutie pas
+        .catch((error) => console.error(error));
+    });
 }
 afficher_object();
