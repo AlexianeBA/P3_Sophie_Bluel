77a361dd552a98658afa84e5a4d05928d67877b6
AlexianeBA <106927033+AlexianeBA@users.noreply.github.com>
2023-05-01 14:29:47 +0200

    add button

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,10 +1,15 @@
+//création de la fonction pour afficher les objects en faisant appel à une API
 function afficher_object() {
+  //requet api avec fonction fetch
   fetch("http://localhost:5678/api/works")
+    //recherche reponse dans le fichier json
     .then((response) => response.json())
+    // si pas de réponse dans JSON alors on va chercher dans l'api
     .then((data) => {
       // Traiter les données JSON
       //définir les éléments qu'on va utiliser présents dans l'api
       const data_response = data;
+
       const gallery = document.getElementById("gallery");
       data_response
         // pour chaque éléments on donne le chemin vers html
@@ -26,20 +31,37 @@ function afficher_object() {
           figure.appendChild(figcaption);
           // on attribut à gallery son enfant figure
           gallery.appendChild(figure);
-        })
-        //afficher error si la recherche dans l'api n'aboutie pas
-        .catch((error) => console.error(error));
+        });
+      //afficher error si la recherche dans l'api n'aboutie pas
+      // .catch((error) => console.error(error));
     });
 }
 afficher_object();
 
-// ajout des filtres
-const boutonTrier = document.querySelector(".btn.filter");
+// function button_filter() {
+//   fetch("http://localhost:5678/api/categories")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const data_response = data;
+//       const portfolio = document.getElementById("button_section");
+//       data_response.forEach((element) => {
+//         var categories_button = document.createElement("button");
 
-boutinTrier.addEventListenner("click", function(){
-  const ordergallery = Array.from(gallery);
-  ordergallery.sort(function(a,b){
-return a.title != b.title;
+//         categories_button.textContent = element.name;
+//         categories_button.name = element.name;
+//         categories_button.className = "btn-filter";
+
+//         portfolio.appendChild(categories_button);
+//       });
+//     });
+// }
+// button_filter();
+
+const btnFilters = document.querySelectorAll(".btn-filter");
+console.log(btnFilters); // affiche une NodeList des boutons avec la classe 'btn-filter'
+
+btnFilters.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.name);
   });
-  console.log(ordergallery);
 });
